e93a66bfaeea18647a7603114c5f73f3e7dfd8ed
ivo <ivo@CCI-LPT-64>
2020-05-30 04:12:52 +0530

    Implemented functionality to list Todo Items

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React,{Component} from 'react';
+import Todos from './components/Todos';
+import './App.css';
 
+class App extends Component {
 
-function App() {
+  state ={
+    todos:[
+      {
+        id:1,
+        title:'Click Passport Photo',
+        completed: false
+      },
+      {
+        id:2,
+        title:'Take xerox of Pan Card and Aadharcard',
+        completed:false
+      },
+      {
+        id:3,
+        title:'Pay ICICI Credit card bill',
+        completed:false
+      },
+    ]
+  }
+  render(){
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <Todos todos={this.state.todos}/>
     </div>
   );
+}
 }
 
 export default App;
